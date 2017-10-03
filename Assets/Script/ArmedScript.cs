540c87dc09a1c8c1f15ffc39defe9aa65f047eab
KirillAlekseenko <kir967960@yandex.ru>
2017-10-03 10:53:52 +0300

    alpha is over

--- Assets/Script/ArmedScript.cs
+++ Assets/Script/ArmedScript.cs
@@ -4,25 +4,43 @@ using System.Collections.Generic;
 
 public class ArmedScript : MonoBehaviour {
 
+	[SerializeField]
 	public List<GameObject> targets;
 
 	private List<GunScript> targetComponents;
 
 	[SerializeField]
+	private GameObject traceParticlePrefab;
 	private GameObject traceParticle;
+	private ParticleSystem traceComponent;
+	private bool isShooting = false;
+	[SerializeField]
+	private float scatter;
 
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < targets.Count; i++) {
 			targetComponents [i] = targets [i].GetComponent<GunScript> ();
 		}
+		startShooting ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
+		if (isShooting) {
+			for (int i = 0; i < targets.Count; i++) {
+				if (targetComponents [i].isFreeze ())
+					continue;
+				shoot (targets [i].transform.position, scatter); 
+			}
+		}
 	
+	}
+
+	void OnDestroy()
+	{
+		finishShooting ();
 	}
 
 	private void shoot(Vector3 target, float scatter)
@@ -33,8 +51,27 @@ public class ArmedScript : MonoBehaviour {
 
 		Vector3 targetWithDeflection = target + deflection * scatter;
 
-		//Instantiate traceParticle
+		float length = (targetWithDeflection - transform.position).magnitude; // length of the particle
+		traceComponent.startLifetime = length / traceComponent.startSpeed;
 
-		// line renderer, particle system, whatever ....
+		Quaternion rotation = Quaternion.LookRotation (targetWithDeflection - transform.position); // particle's rotation
+		traceParticle.transform.localRotation = rotation;
+		traceComponent.startRotation3D = new Vector3 (0, 0, 180.0f + rotation.y);
+
+
+	}
+	private void startShooting()
+	{
+		isShooting = true;
+		traceParticle = Instantiate (traceParticlePrefab, transform.position, Quaternion.Euler(180.0f, 0, 0)) as GameObject;
+		traceComponent = traceParticle.GetComponent<ParticleSystem> ();
+	}
+	private void finishShooting()
+	{
+		isShooting = false;
+		if(traceParticle != null)
+			Destroy (traceParticle);
+		traceParticle = null;
+		traceComponent = null;
 	}
 }
